bd8b0399b5c615b3337a4c2d5f57958e78dcb4bd
E216351P <Ethan27349570!>
2022-06-07 16:07:37 +0200

    BotButton
    Signed-off-by: E216351P <Ethan27349570!>

--- src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
+++ src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
@@ -2,14 +2,10 @@ package projet.echecmartien
 
 import javafx.application.Application
 import javafx.scene.Scene
-import javafx.scene.control.Label
-import javafx.scene.image.Image
 import javafx.scene.layout.*
 
 import javafx.stage.Stage
 import projet.echecmartien.controleurs.ControleurBot
-import projet.echecmartien.controleurs.ControleurRetour
-import projet.echecmartien.controleurs.ControleurRules
 import projet.echecmartien.modele.*
 import projet.echecmartien.vue.MainVue
 
@@ -17,7 +13,7 @@ class AppliJeuEchecMartien: Application() {
     override fun start(primaryStage: Stage) {
         val vue = MainVue()
         vue.fixeListenerBouton(vue.botButton, ControleurBot(vue))
-        vue.fixeListenerBouton(vue.rulesButton, ControleurRules(vue))
+
         //val img = Image("https://cdn-s-www.lejsl.com/images/B03DA098-E2DA-4F6A-937A-6AE2D6700B6D/NW_raw/la-galaxie-dans-laquelle-le-fluor-a-ete-detecte-se-trouve-a-12-milliards-d-annees-lumiere-image-d-illustration-pixabay-1636110175.jpg")
         //val bImg = BackgroundImage(img, BackgroundRepeat.NO_REPEAT,
         //    BackgroundRepeat.NO_REPEAT,
