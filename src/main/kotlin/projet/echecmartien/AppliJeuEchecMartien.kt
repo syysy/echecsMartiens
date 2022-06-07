ca7e86054f6a67c0a2d219ec24630e07175b790a
E216351P <Ethan27349570!>
2022-06-07 23:44:32 +0200

    Modif start pour changment de vues
    Signed-off-by: E216351P <Ethan27349570!>

--- src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
+++ src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
@@ -2,6 +2,7 @@ package projet.echecmartien
 
 import javafx.application.Application
 import javafx.scene.Scene
+import javafx.scene.layout.BorderPane
 
 import javafx.stage.Stage
 import projet.echecmartien.controleurs.ControleurBot
@@ -11,32 +12,43 @@ import projet.echecmartien.vue.MainVue
 
 class AppliJeuEchecMartien: Application() {
     override fun start(primaryStage: Stage) {
-        val vue = JeuVue()
-        /*vue.fixeListenerBouton(vue.botButton, ControleurBot(vue))
-        vue.fixeListenerBouton(vue.rulesButton,ControleurRules(vue))
-        //val img = Image("https://cdn-s-www.lejsl.com/images/B03DA098-E2DA-4F6A-937A-6AE2D6700B6D/NW_raw/la-galaxie-dans-laquelle-le-fluor-a-ete-detecte-se-trouve-a-12-milliards-d-annees-lumiere-image-d-illustration-pixabay-1636110175.jpg")
-        //val bImg = BackgroundImage(img, BackgroundRepeat.NO_REPEAT,
-        //    BackgroundRepeat.NO_REPEAT,
-        //    BackgroundPosition.CENTER,
-        //    BackgroundSize.DEFAULT)
-        //val background = Background(bImg)
-        //vue.background = background
-        vue.rulesButton.setOnAction {
-            primaryStage.height = 500.0
-            primaryStage.width = 1080.0
-            primaryStage.centerOnScreen()
-            vue.rulesUpdate()
+        var boolVue = false
+        for (i in 2 downTo 0){
+            if (!boolVue){
+                val vue = MainVue()
+                vue.fixeListenerBouton(vue.botButton,ControleurBot(vue))
+                vue.fixeListenerBouton(vue.rulesButton,ControleurRules(vue))
+
+                vue.rulesButton.setOnAction {
+                    primaryStage.height = 500.0
+                    primaryStage.width = 1080.0
+                    primaryStage.centerOnScreen()
+                    vue.rulesUpdate()
+                }
+                vue.buttonBottomRules.setOnAction {
+                    primaryStage.height = 500.0
+                    primaryStage.width = 400.0
+                    primaryStage.centerOnScreen()
+                    vue.originUpdate()
+                }
+                vue.playButton.setOnAction {
+                    boolVue = true
+                }
+
+                val scene = Scene(vue,350.0,500.0)
+                primaryStage.title="TD5B MVC"
+                primaryStage.scene=scene
+                primaryStage.show()
+            }else{
+                val vue = JeuVue()
+                val scene = Scene(vue,350.0,700.0)
+                primaryStage.title="TD5B MVC"
+                primaryStage.scene=scene
+                primaryStage.show()
+            }
         }
-        vue.buttonBottomRules.setOnAction {
-            primaryStage.height = 500.0
-            primaryStage.width = 400.0
-            primaryStage.centerOnScreen()
-            vue.originUpdate()
-         }*/
-        val scene = Scene(vue,350.0,500.0)
-        primaryStage.title="TD5B MVC"
-        primaryStage.scene=scene
-        primaryStage.show()
+
+
     }
 }
 
@@ -44,6 +56,12 @@ fun main() {
     Application.launch(AppliJeuEchecMartien::class.java)
 }
 
-
+//val img = Image("https://cdn-s-www.lejsl.com/images/B03DA098-E2DA-4F6A-937A-6AE2D6700B6D/NW_raw/la-galaxie-dans-laquelle-le-fluor-a-ete-detecte-se-trouve-a-12-milliards-d-annees-lumiere-image-d-illustration-pixabay-1636110175.jpg")
+//val bImg = BackgroundImage(img, BackgroundRepeat.NO_REPEAT,
+//    BackgroundRepeat.NO_REPEAT,
+//    BackgroundPosition.CENTER,
+//    BackgroundSize.DEFAULT)
+//val background = Background(bImg)
+//vue.background = background
 
 
