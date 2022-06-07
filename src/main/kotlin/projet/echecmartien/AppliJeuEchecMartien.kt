855a2a4787a6be028ef1bf5ea91ab0ddb71c8586
E216351P <Ethan27349570!>
2022-06-07 21:37:08 +0200

    VuePlateau update
    Signed-off-by: E216351P <Ethan27349570!>

--- src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
+++ src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
@@ -6,12 +6,13 @@ import javafx.scene.Scene
 import javafx.stage.Stage
 import projet.echecmartien.controleurs.ControleurBot
 import projet.echecmartien.controleurs.ControleurRules
+import projet.echecmartien.vue.JeuVue
 import projet.echecmartien.vue.MainVue
 
 class AppliJeuEchecMartien: Application() {
     override fun start(primaryStage: Stage) {
-        val vue = MainVue()
-        vue.fixeListenerBouton(vue.botButton, ControleurBot(vue))
+        val vue = JeuVue()
+        /*vue.fixeListenerBouton(vue.botButton, ControleurBot(vue))
         vue.fixeListenerBouton(vue.rulesButton,ControleurRules(vue))
         //val img = Image("https://cdn-s-www.lejsl.com/images/B03DA098-E2DA-4F6A-937A-6AE2D6700B6D/NW_raw/la-galaxie-dans-laquelle-le-fluor-a-ete-detecte-se-trouve-a-12-milliards-d-annees-lumiere-image-d-illustration-pixabay-1636110175.jpg")
         //val bImg = BackgroundImage(img, BackgroundRepeat.NO_REPEAT,
@@ -31,7 +32,7 @@ class AppliJeuEchecMartien: Application() {
             primaryStage.width = 400.0
             primaryStage.centerOnScreen()
             vue.originUpdate()
-         }
+         }*/
         val scene = Scene(vue,350.0,500.0)
         primaryStage.title="TD5B MVC"
         primaryStage.scene=scene
--- src/main/kotlin/projet/echecmartien/vue/JeuVue.kt
+++ src/main/kotlin/projet/echecmartien/vue/JeuVue.kt
@@ -1,17 +1,19 @@
 package projet.echecmartien.vue
 
 import javafx.geometry.Insets
+import javafx.geometry.Pos
 import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.control.TextField
-import javafx.scene.layout.BorderPane
-import javafx.scene.layout.GridPane
-import javafx.scene.layout.HBox
-import javafx.scene.layout.VBox
+import javafx.scene.layout.*
+import javafx.scene.paint.Color
 import javafx.scene.shape.Circle
+import javafx.scene.text.Font
+import javafx.scene.text.FontPosture
+import javafx.scene.text.FontWeight
 
 class JeuVue() : BorderPane() {
-    val titre = Label("Echecs Martiens")
+    var labelTop = Label("Echecs Martiens")
 
     val centre = VBox()
     val info1 = HBox()
@@ -41,21 +43,47 @@ class JeuVue() : BorderPane() {
     val boutonReset = Button("Reset")
 
     init{
-        this.top = titre
+        // Titre en haut
+        val flowPaneTop = FlowPane()
+        labelTop.font = Font.font("Tahoma", FontWeight.BOLD, FontPosture.REGULAR, 20.0)
+        labelTop.textFill = Color.BLACK
+        flowPaneTop.alignment = Pos.CENTER
+        labelTop.padding = Insets(30.0,0.0,30.0,0.0)
+        flowPaneTop.children.add(labelTop)
+        this.top = flowPaneTop
+
         this.pions1.children.addAll(grand1,moyen1,petit1)
         this.pions2.children.addAll(grand2,moyen2,petit2)
         this.left = pions1
         this.right = pions2
+
+        // Centre
         info1.children.addAll(joueur1,point1)
+        info1.spacing = 160.0
+        info1.padding = Insets(10.0)
         info2.children.addAll(joueur2,point2)
+        info2.spacing = 160.0
+        info2.padding = Insets(10.0)
         centre.children.addAll(info1,grille,info2)
+        centre.alignment = Pos.CENTER
+        centre.padding = Insets(0.0,30.0,30.0,30.0)
         this.center = centre
+
+
+        // Boutons du bas
+        compteTour.style = " -fx-font-size : 15 ;-fx-font-weight :bold"
         bot.add(compteTour,1,0)
         bot.add(boutonCharge,0,1)
         bot.add(boutonSave,0,2)
         bot.add(boutonRegles,2,1)
         bot.add(boutonReset,2,2)
+        bot.vgap = 20.0
+        bot.hgap = 50.0
+        bot.padding = Insets(30.0)
+        bot.alignment = Pos.CENTER
         this.bottom = bot
+
+
         for (i in 0 until 4){
             for (j in 0 until 8){
                 val case = Button()
