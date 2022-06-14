7bdbbdf85a44315a679b3b2669f763a288bd9453
E211459X <mathis.caprin@etu.univ-nantes.fr>
2022-06-14 13:38:18 +0200

    get

--- src/main/kotlin/projet/echecmartien/vue/JeuVue.kt
+++ src/main/kotlin/projet/echecmartien/vue/JeuVue.kt
@@ -7,6 +7,7 @@ import javafx.geometry.Insets
 import javafx.geometry.Pos
 import javafx.scene.Group
 import javafx.scene.Node
+import javafx.scene.control.Alert
 import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.control.TextField
@@ -165,7 +166,7 @@ class JeuVue() : BorderPane() {
 
                 }
             }
-
+            val alert : Alert
         }
     }
 
