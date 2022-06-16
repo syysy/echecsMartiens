d7a9c99dc1a39b3bc843daaad74c4d7ce634ef04
E211439A <L4v3r1T3>
2022-06-16 20:25:10 +0200

    Tentative de style sur la victoire

--- src/main/kotlin/projet/echecmartien/modele/GrandPion.kt
+++ src/main/kotlin/projet/echecmartien/modele/GrandPion.kt
@@ -8,6 +8,11 @@ open class GrandPion : Pion(){
         return 3
     }
 
+    /**
+     * Regarde que le déplacement est sur le plateau et qu'il est soit horizontal, soit vertical, soit
+     * diagonale. Dans le cas où il ne remplis pas les conditions, renvoie une erreur, sinon effectue le
+     * déplacement.
+     * */
     override fun getDeplacement(deplacement: Deplacement): List<Coordonnee> {
         if (deplacement.getOrigine().getX() !in 0..3 || deplacement.getOrigine().getY() !in 0..7 || deplacement.getDestination().getX() !in 0..3 || deplacement.getDestination().getY() !in 0..7){
             throw DeplacementException()
